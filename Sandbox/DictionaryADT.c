ee07b04aab4bd3ade1c8795633d9c6a7f4158fc1
Muchen He <mansurhe@outlook.com>
2016-11-06 15:14:06 -0800

    added materials for lab 4

---
+++ Sandbox/DictionaryADT.c
@@ -0,0 +1,7 @@
+// dictionary contains two types
+// two main functions: -insert and -find
+
+// store values associated with user specififed keyPressed
+// values may be any type (homogeneous)
+// keys may be any type (homogeneous) and must be comparable
+// -- if the key is not comparable, then the serach is O(n) 
