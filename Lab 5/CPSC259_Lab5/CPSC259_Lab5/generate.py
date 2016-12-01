3a8e92b5525f25fc1cef489d92ac793961619132
Muchen He <mansurhe@outlook.com>
2016-12-01 10:31:45 -0800

    added python file to generate random weblink txt file

---
+++ Lab 5/CPSC259_Lab5/CPSC259_Lab5/generate.py
@@ -0,0 +1,21 @@
+# generates a random series of webpages
+# -- square matrix
+# -- main diagonal is all 0s
+# -- output as .txt file
+
+import random
+
+N = int(input("Enter number of links: "))
+
+# write to file
+file = open("weblink.txt", "w")
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            file.write("0")
+        else:
+            file.write(str(random.choice(["0", "1"])))
+        if (j != N - 1):
+            file.write(" ")
+    file.write("\n")
+file.close()
