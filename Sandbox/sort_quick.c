e3dffcf687cddf2089995a7bf6e8de7dbce8f8e1
Muchen He <mansurhe@outlook.com>
2016-12-10 13:23:07 -0800

    started quicksort

--- Sandbox/sort_quick.c
+++ Sandbox/sort_quick.c
@@ -1,1 +1,5 @@
 // find and return the index of pivot element
+// 1. pick a pivot
+// 2. all elements < pivot reordered to left
+//    all elements >= pivot reordered to right
+// 3. recursively sort each side
